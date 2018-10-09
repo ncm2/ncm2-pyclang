ncm2/ncm2-pyclang

commit eefa42d51df4e89cde5dfa4c9f34dbc7c9453f82

fix #13 for now

pythonx/ncm2_pyclang_proc.py
@@ -230,6 +230,8 @@ class Source(Ncm2Source):
         CXTranslationUnit_KeepGoing = 0x200
         CXTranslationUnit_CreatePreambleOnFirstParse = 0x100
 
+        args.insert(0, '-working-directory=' + directory)
+
         if not for_completion:
             flags = cindex.TranslationUnit.PARSE_PRECOMPILED_PREAMBLE | \
                 cindex.TranslationUnit.PARSE_INCOMPLETE | \

commit c3cd95ed31dbe1188f7b5be5fb65abed310c1f26

fix performance issue caused by eefa42d
Modifying the list directly will invalidate the cache check. Create a
new list instead.

pythonx/ncm2_pyclang_proc.py
@@ -228,7 +228,7 @@ class Source(Ncm2Source):
         CXTranslationUnit_KeepGoing = 0x200
         CXTranslationUnit_CreatePreambleOnFirstParse = 0x100
 
-        args.insert(0, '-working-directory=' + directory)
+        args = ['-working-directory='] + args
 
         if not for_completion:
             flags = cindex.TranslationUnit.PARSE_PRECOMPILED_PREAMBLE | \
@@ -352,10 +352,10 @@ class Source(Ncm2Source):
 
         inc_match = self.include_pat.search(typed)
         if inc_match:
-            matches = self.get_include_completions(data,
-                                                   args,
-                                                   directory,
-                                                   inc_match.group(1))
+            self.get_include_completions(data,
+                                         args,
+                                         directory,
+                                         inc_match.group(1))
             return
 
         start = time.time()